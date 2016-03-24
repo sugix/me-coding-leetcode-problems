9cd654325ead28e49bbb2ec660321bb161c69a83
Sugi Venugeethan <sugi205@gmail.com>
2016-03-24 22:48:36 +0000

    [LeetCode Sync] Runtime - 12 ms (31.66%), Memory - N/A (N/A)

---
+++ my-solutions/0006-zigzag-conversion/solution.java
@@ -0,0 +1,30 @@
+import java.lang.*;
+
+public class Solution {
+    public String convert(String s, int numRows) {
+        if(numRows <= 1)
+            return s;
+        StringBuilder[] list = new StringBuilder[numRows];
+        for(int i = 0; i < numRows; i++)
+            list[i] = new StringBuilder();
+        int row = 0;
+        int i = 0;
+        boolean down = true;
+        while(i < s.length()){
+            list[row].append(s.charAt(i));
+            if(row == 0)
+                down = true;
+            if(row == numRows - 1)
+                down = false;
+            if(down)
+                row++;
+            else
+                row--;
+            i++;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder sb : list)
+            res.append(sb.toString());
+        return res.toString();
+    }
+}
