5abe301f68016bad3720286f621206483aa9d2b1
Sugi Venugeethan <sugi205@gmail.com>
2016-03-29 03:55:14 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - N/A (N/A)

---
+++ my-solutions/0024-swap-nodes-in-pairs/solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if(head == null)
+            return head;
+            
+        if(head.next == null)    
+            return head;
+            
+            
+        ListNode p = head;            
+        while(p != null){
+            if(p.next != null){
+                int currentValue = p.val;
+                p.val = p.next.val;
+                p.next.val = currentValue;
+            }
+            if(p.next != null)
+                p = p.next.next;
+            else
+                p = p.next;
+        }            
+            
+        return head;
+    }
+}
