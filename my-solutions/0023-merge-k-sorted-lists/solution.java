5dcaad86441ab26be0eed27e0790a1b2dd2eed1e
Sugi Venugeethan <sugi205@gmail.com>
2016-03-29 03:17:28 +0000

    [LeetCode Sync] Runtime - 11 ms (19.15%), Memory - N/A (N/A)

---
+++ my-solutions/0023-merge-k-sorted-lists/solution.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length == 0)
+            return null;
+        
+        Comparator<ListNode> c = (a,b) -> {
+          return a.val > b.val ? +1 : a.val < b.val ? -1 : 0;  
+        };
+        
+        Queue<ListNode> processingQueue = new PriorityQueue<>(lists.length,c);
+        
+        // Add the head of all lists to the processingQueue
+        for(ListNode list: lists){
+            if(list != null)
+                processingQueue.add(list);
+        }
+        
+        // Create a dummy head node
+        ListNode head = new ListNode(0);
+        ListNode p = head;
+        
+        while(processingQueue.size() > 0){
+            ListNode temp = processingQueue.poll();
+            p.next = temp;
+            
+            if(temp.next != null)
+                processingQueue.add(temp.next);
+            p = p.next;     
+        }
+        
+        return head.next;
+    }
+}
