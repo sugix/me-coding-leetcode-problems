1b890874c40ff8738bf76847db83dc104dd0acac
Sugi Venugeethan <sugi205@gmail.com>
2016-03-31 05:09:07 +0000

    [LeetCode Sync] Runtime - 3 ms (50.43%), Memory - N/A (N/A)

---
+++ my-solutions/0055-jump-game/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums.length <= 1)
+        return true;
+ 
+    int max = nums[0]; //max stands for the largest index that can be reached.
+ 
+    for(int i=0; i<nums.length; i++){
+        //if not enough to go to next
+        if(max <= i && nums[i] == 0) 
+            return false;
+ 
+        //update max    
+        if(i + nums[i] > max){
+            max = i + nums[i];
+        }
+ 
+        //max is enough to reach the end
+        if(max >= nums.length-1) 
+            return true;
+    }
+ 
+    return false;
+    }
+}
