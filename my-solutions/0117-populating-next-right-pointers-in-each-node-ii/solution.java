834b39a67389ea03764a6053026ee37a3740f5c8
Sugi Venugeethan <sugi205@gmail.com>
2016-04-02 00:17:38 +0000

    [LeetCode Sync] Runtime - 2 ms (39.99%), Memory - N/A (N/A)

---
+++ my-solutions/0117-populating-next-right-pointers-in-each-node-ii/solution.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void connect(TreeLinkNode root) {
+        if(root == null) 
+        return;
+ 
+    TreeLinkNode lastHead = root;//prevous level's head 
+    TreeLinkNode lastCurrent = null;//previous level's pointer
+    TreeLinkNode currentHead = null;//currnet level's head 
+    TreeLinkNode current = null;//current level's pointer
+ 
+    while(lastHead!=null){
+        lastCurrent = lastHead;
+ 
+        while(lastCurrent!=null){
+            //left child is not null
+            if(lastCurrent.left!=null)    {
+                if(currentHead == null){
+                    currentHead = lastCurrent.left;
+                    current = lastCurrent.left;
+                }else{
+                    current.next = lastCurrent.left;
+                    current = current.next;
+                }
+            }
+ 
+            //right child is not null
+            if(lastCurrent.right!=null){
+                if(currentHead == null){
+                    currentHead = lastCurrent.right;
+                    current = lastCurrent.right;
+                }else{
+                    current.next = lastCurrent.right;
+                    current = current.next;
+                }
+            }
+ 
+            lastCurrent = lastCurrent.next;
+        }
+ 
+        //update last head
+        lastHead = currentHead;
+        currentHead = null;
+    }
+    }
+}
