32c26b1633fb4f0e931bb25e5bdb00c37e48aab0
Sugi Venugeethan <sugi205@gmail.com>
2016-04-02 02:09:07 +0000

    [LeetCode Sync] Runtime - 3 ms (7.21%), Memory - N/A (N/A)

---
+++ my-solutions/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int maxProfit(int[] prices) {
+        
+        if(prices.length < 2)
+            return 0;
+            
+        int profit = 0;
+        int minElement = Integer.MAX_VALUE;
+    for(int i=0; i<prices.length; i++){
+       profit = Math.max(profit, prices[i]-minElement);
+       minElement = Math.min(minElement, prices[i]);
+    }
+        return profit;
+    }
+}
