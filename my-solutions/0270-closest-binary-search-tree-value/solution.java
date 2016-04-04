901ed848c40865a5af570f3abfbf930501bc0780
Sugi Venugeethan <sugi205@gmail.com>
2016-04-04 18:43:10 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - N/A (N/A)

---
+++ my-solutions/0270-closest-binary-search-tree-value/solution.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private double min = Double.MAX_VALUE;
+    private int ans = 0;
+    
+    public int closestValue(TreeNode root, double target) {
+        if (root == null) {
+            return Integer.MAX_VALUE;
+        }
+         
+        closestValueHelper(root, target);
+         
+        return ans;    
+    }
+    
+    private void closestValueHelper(TreeNode root, double target) {
+        if (root == null) {
+            return;
+        }
+         
+        if (Math.abs((double) root.val - target) < min) {
+            min = Math.abs((double) root.val - target);
+            ans = root.val;
+        }
+         
+        if (root.val > target) {
+            closestValueHelper(root.left, target);
+        } else if (root.val < target) {
+            closestValueHelper(root.right, target);
+        }
+    }
+}
