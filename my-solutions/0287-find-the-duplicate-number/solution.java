9e8564853259118cd100be973f905fb738260ce4
Sugi Venugeethan <sugi205@gmail.com>
2016-04-04 22:19:44 +0000

    [LeetCode Sync] Runtime - 6 ms (54.28%), Memory - N/A (N/A)

---
+++ my-solutions/0287-find-the-duplicate-number/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+         
+        int lo = 1;
+        int hi = nums.length - 1;
+         
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            int count = countNumbers(nums, mid);
+             
+            if (count <= mid) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+         
+        return hi;
+    }
+    
+    private int countNumbers(int[] nums, int mid) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= mid) {
+                count++;
+            }
+        }
+         
+        return count;
+    }
+}
