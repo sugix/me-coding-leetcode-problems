d5f1e0422a02ed2268c1d1d5e1c52a0ed7e9498a
Sugi Venugeethan <sugi205@gmail.com>
2016-04-04 05:55:31 +0000

    [LeetCode Sync] Runtime - 2 ms (21.06%), Memory - N/A (N/A)

---
+++ my-solutions/0256-paint-house/solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int minCost(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+         
+        int n = costs.length;
+        int[] prev = new int[3];
+        int[] curr = new int[3];
+         
+        prev[0] = costs[0][0];
+        prev[1] = costs[0][1];
+        prev[2] = costs[0][2];
+         
+        for (int i = 1; i < n; i++) {
+            curr[0] = Math.min(prev[1], prev[2]) + costs[i][0];
+            curr[1] = Math.min(prev[0], prev[2]) + costs[i][1];
+            curr[2] = Math.min(prev[0], prev[1]) + costs[i][2];
+             
+            // update the curr
+            prev[0] = curr[0];
+            prev[1] = curr[1];
+            prev[2] = curr[2];
+        }
+         
+        return Math.min(Math.min(prev[0], prev[1]), prev[2]);
+    }
+}
