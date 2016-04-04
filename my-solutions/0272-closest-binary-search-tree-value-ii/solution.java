e66bd2abcccd74dd49295d0151540811436e4119
Sugi Venugeethan <sugi205@gmail.com>
2016-04-04 20:47:28 +0000

    [LeetCode Sync] Runtime - 6 ms (11.53%), Memory - N/A (N/A)

---
+++ my-solutions/0272-closest-binary-search-tree-value-ii/solution.java
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+         
+        Stack<Integer> precedessor = new Stack<>();
+        Stack<Integer> successor = new Stack<>();
+         
+        getPredecessor(root, target, precedessor);
+        getSuccessor(root, target, successor);
+         
+        for (int i = 0; i < k; i++) {
+            if (precedessor.isEmpty()) {
+                result.add(successor.pop());
+            } else if (successor.isEmpty()) {
+                result.add(precedessor.pop());
+            } else if (Math.abs((double) precedessor.peek() - target) < Math.abs((double) successor.peek() - target)) {
+                result.add(precedessor.pop());
+            } else {
+                result.add(successor.pop());
+            }
+        }
+         
+        return result;
+    }
+    
+    private void getPredecessor(TreeNode root, double target, Stack<Integer> precedessor) {
+        if (root == null) {
+            return;
+        }
+         
+        getPredecessor(root.left, target, precedessor);
+         
+        if (root.val > target) {
+            return;
+        }
+         
+        precedessor.push(root.val);
+         
+        getPredecessor(root.right, target, precedessor);
+    }
+    
+    private void getSuccessor(TreeNode root, double target, Stack<Integer> successor) {
+        if (root == null) {
+            return;
+        }
+         
+        getSuccessor(root.right, target, successor);
+         
+        if (root.val <= target) {
+            return;
+        }
+         
+        successor.push(root.val);
+         
+        getSuccessor(root.left, target, successor);
+    }
+}
