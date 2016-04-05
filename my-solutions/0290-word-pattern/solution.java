39ad409d1a03e8a5013a07e51acb15c98e4f9be4
Sugi Venugeethan <sugi205@gmail.com>
2016-04-05 02:14:24 +0000

    [LeetCode Sync] Runtime - 3 ms (6.73%), Memory - N/A (N/A)

---
+++ my-solutions/0290-word-pattern/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        if (pattern == null || pattern.length() == 0 || str == null || str.length() == 0) {
+            return false;
+        }
+ 
+        String[] tokens = str.split(" ");
+         
+        if (pattern.length() != tokens.length) {
+            return false;
+        }
+ 
+        Map<String, Character> inverseMap = new HashMap<>();
+        Map<Character, String> map = new HashMap();
+         
+        int i = 0;
+        for (i = 0; i < pattern.length(); i++) {
+            char digit = pattern.charAt(i);
+             
+            String token = tokens[i];
+             
+            // Check the one-one mapping
+            if (!map.containsKey(digit) && !inverseMap.containsKey(token)) {
+                map.put(digit, token);
+                inverseMap.put(token, digit);
+            } else if (map.containsKey(digit) && inverseMap.containsKey(token)) {
+                String token1 = map.get(digit);
+                char digit1 = inverseMap.get(token);
+                 
+                if (!token1.equals(token) || digit != digit1) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+         
+        return true;
+    }
+}
