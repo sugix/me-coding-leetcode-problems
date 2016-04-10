f80eae8d4bd43fa5d6078d9694ce1e43101abb5b
Sugi Venugeethan <sugi205@gmail.com>
2016-04-10 21:54:29 +0000

    [LeetCode Sync] Runtime - 3 ms (99.88%), Memory - N/A (N/A)

---
+++ my-solutions/0077-combinations/solution.java
@@ -0,0 +1,27 @@
+public class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+ 
+	if (n <= 0 || n < k)
+		return result;
+ 
+	List<Integer> item = new ArrayList<>();
+	dfs(n, k, 1, item, result); // because it need to begin from 1
+ 
+	return result;
+    }
+    
+    private void dfs(int n, int k, int start, List<Integer> item,
+		List<List<Integer>> res) {
+	if (item.size() == k) {
+		res.add(new ArrayList<Integer>(item));
+		return;
+	}
+ 
+	for (int i = start; i <= n; i++) {
+		item.add(i);
+		dfs(n, k, i + 1, item, res);
+		item.remove(item.size() - 1);
+	}
+}
+}
