06ad56dde683191471883e8defa0766d135ecdc4
Sugi Venugeethan <sugi205@gmail.com>
2016-04-10 22:02:51 +0000

    [LeetCode Sync] Runtime - 1 ms (24.46%), Memory - N/A (N/A)

---
+++ my-solutions/0216-combination-sum-iii/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+    List<Integer> list = new ArrayList<Integer>();
+    dfs(result, 1, n, list, k);
+    return result;
+    }
+    
+    public void dfs(List<List<Integer>> result, int start, int sum, List<Integer> list, int k){
+    if(sum==0 && list.size()==k){
+        List<Integer> temp = new ArrayList<Integer>();
+        temp.addAll(list);
+        result.add(temp);
+    }
+ 
+    for(int i=start; i<=9; i++){
+        if(sum-i<0) break;
+        if(list.size()>k) break;
+ 
+        list.add(i);
+        dfs(result, i+1, sum-i, list, k);
+        list.remove(list.size()-1);
+    }
+}
+}
