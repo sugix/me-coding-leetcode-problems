71844357fea8bed9a146680e09a1d1fb6243f968
Sugi Venugeethan <sugi205@gmail.com>
2016-04-10 00:30:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - N/A (N/A)

---
+++ my-solutions/0035-search-insert-position/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if(nums==null||nums.length==0)
+            return 0;
+ 
+        return searchInsert(nums,target,0,nums.length-1);
+    }
+    
+    public int searchInsert(int[] A, int target, int start, int end){
+        int mid=(start+end)/2;
+ 
+        if(target==A[mid]) 
+            return mid;
+        else if(target<A[mid]) 
+            return start<mid?searchInsert(A,target,start,mid-1):start;
+        else 
+            return end>mid?searchInsert(A,target,mid+1,end):(end+1);
+    }
+}
