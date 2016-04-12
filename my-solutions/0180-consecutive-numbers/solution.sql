0101c1a400626547dd027d5d877ab829167ce087
Sugi Venugeethan <sugi205@gmail.com>
2016-04-12 03:37:05 +0000

    [LeetCode Sync] Runtime - 1599 ms (5.00%), Memory - N/A (N/A)

---
+++ my-solutions/0180-consecutive-numbers/solution.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+#select Num from (select Num,count(distinct Num) as cnt from Logs)a where a.cnt>=3;
+
+SELECT  DISTINCT(Num) AS ConsecutiveNums
+FROM (
+    SELECT
+    Num,
+    @counter := IF(@prev = Num, @counter + 1, 1) AS how_many_cnt_in_a_row,
+    @prev := Num
+    FROM Logs y, (SELECT @counter:=1, @prev:=NULL) vars
+) sq
+WHERE how_many_cnt_in_a_row >= 3
