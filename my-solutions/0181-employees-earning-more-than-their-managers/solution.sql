6b5074709eaacc0e277f86e2c68bbbf4f2cd948a
Sugi Venugeethan <sugi205@gmail.com>
2016-04-12 03:48:11 +0000

    [LeetCode Sync] Runtime - 1444 ms (5.03%), Memory - N/A (N/A)

---
+++ my-solutions/0181-employees-earning-more-than-their-managers/solution.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+
+SELECT Name AS Employee 
+    FROM Employee e 
+    WHERE e.ManagerId IS NOT NULL AND e.Salary > (SELECT Salary 
+                          FROM Employee 
+                          WHERE e.ManagerId = Id)
+
