3048fc179bc7294a7e022e888a825d8ea29ab7df
Sugi Venugeethan <sugi205@gmail.com>
2016-04-18 07:32:33 +0000

    [LeetCode Sync] Runtime - 5 ms (7.90%), Memory - N/A (N/A)

---
+++ my-solutions/0152-maximum-product-subarray/solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int maxProduct(int[] nums) {
+       if(nums==null || nums.length==0)  
+        return 0;  
+ 
+    int maxLocal = nums[0];  
+    int minLocal = nums[0];  
+    int global = nums[0];  
+ 
+    for(int i=1; i<nums.length; i++){  
+        int temp = maxLocal;  
+        maxLocal = Math.max(Math.max(nums[i]*maxLocal, nums[i]), nums[i]*minLocal);  
+        minLocal = Math.min(Math.min(nums[i]*temp, nums[i]), nums[i]*minLocal);  
+        global = Math.max(global, maxLocal);  
+    }  
+    return global; 
+    }
+}
