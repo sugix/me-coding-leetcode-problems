dad418fbfa7694ab698ffe2b729b5e0c54bd27c2
Sugi Venugeethan <sugi205@gmail.com>
2016-04-18 05:18:28 +0000

    [LeetCode Sync] Runtime - 14 ms (5.05%), Memory - N/A (N/A)

---
+++ my-solutions/0240-search-a-2d-matrix-ii/solution.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+         
+        int m = matrix.length;
+        int n = matrix[0].length;
+         
+        int row = m - 1;
+        int col = 0;
+         
+        while (row >= 0 && col < n) {
+            if (target == matrix[row][col]) {
+                return true;
+            } else if (target < matrix[row][col]) {
+                row--;
+            } else {
+                col++;
+            }
+        }
+        return false;
+    }
+}
