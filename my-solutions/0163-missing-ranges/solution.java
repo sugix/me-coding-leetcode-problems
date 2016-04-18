41cabb051c8e6031898e40a440ba08c01b853621
Sugi Venugeethan <sugi205@gmail.com>
2016-04-18 07:40:11 +0000

    [LeetCode Sync] Runtime - 1 ms (100.00%), Memory - N/A (N/A)

---
+++ my-solutions/0163-missing-ranges/solution.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> result = new ArrayList<String>();
+        if (nums == null || nums.length == 0) {
+            outputToResult(lower, upper, result);
+            return result;
+        }
+         
+        // leading missing range
+        if (nums[0] - lower > 0) {
+            outputToResult(lower, nums[0] - 1, result);
+        }
+         
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] - nums[i - 1] > 1) {
+                outputToResult(nums[i - 1] + 1, nums[i] - 1, result);
+            }
+        }
+         
+        // trailing missage ranges
+        if (upper - nums[nums.length - 1] > 0) {
+            outputToResult(nums[nums.length - 1] + 1, upper, result);
+        }
+         
+        return result;
+    }
+    
+    private void outputToResult(int start, int end, List<String> result) {
+        StringBuffer sb = new StringBuffer();
+        if (start == end) {
+            sb.append(start);
+        } else {
+            sb.append(start + "->" + end);
+        }
+         
+        result.add(sb.toString());
+    }
+}
