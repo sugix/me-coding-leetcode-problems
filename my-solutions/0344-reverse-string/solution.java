c04d93673142eda82a418847195568bda9961bff
Sugi Venugeethan <sugi205@gmail.com>
2016-04-23 22:51:51 +0000

    [LeetCode Sync] Runtime - 3 ms (8.63%), Memory - N/A (N/A)

---
+++ my-solutions/0344-reverse-string/solution.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public String reverseString(String s) {
+        if(s == null)
+            return null;
+            
+        char[] sa = s.toCharArray();
+        
+        for(int i = 0, j = sa.length - 1; i < sa.length / 2; i++, j--) {
+            char c = sa[i];
+            sa[i] = sa[j];
+            sa[j] = c;
+        }
+        
+        return new String(sa);
+    }
+}
