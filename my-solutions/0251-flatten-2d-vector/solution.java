c6bba4b3938d2c54b3690d2b33fd4e926ef4b54e
Sugi Venugeethan <sugi205@gmail.com>
2016-04-24 04:00:37 +0000

    [LeetCode Sync] Runtime - 5 ms (100.00%), Memory - N/A (N/A)

---
+++ my-solutions/0251-flatten-2d-vector/solution.java
@@ -0,0 +1,37 @@
+public class Vector2D implements Iterator<Integer> {
+    
+    private Iterator<List<Integer>>outerIterator;
+    private Iterator<Integer> innerIterator;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        outerIterator = vec2d.iterator();
+        innerIterator = Collections.emptyIterator();
+    }
+
+    @Override
+    public Integer next() {
+       return innerIterator.next(); 
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (innerIterator.hasNext()) {
+            return true;
+        }
+         
+        if (!outerIterator.hasNext()) {
+            return false;
+        }
+         
+        innerIterator = outerIterator.next().iterator();
+         
+         
+        return hasNext();
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D i = new Vector2D(vec2d);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
