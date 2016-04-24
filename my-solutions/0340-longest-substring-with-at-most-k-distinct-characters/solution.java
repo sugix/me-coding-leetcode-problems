f3435d0212130e92c948c6f9bf921b8c0e383b08
Sugi Venugeethan <sugi205@gmail.com>
2016-04-24 04:22:16 +0000

    [LeetCode Sync] Runtime - 69 ms (5.46%), Memory - N/A (N/A)

---
+++ my-solutions/0340-longest-substring-with-at-most-k-distinct-characters/solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        if (s == null || s.length() == 0 || k <= 0) {
+            return 0;
+        }
+        int start = 0;
+        int res = 1;
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        map.put(s.charAt(0), 1);
+        for (int end = 1; end < s.length(); end++) {
+            char ch = s.charAt(end);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch)+1);
+            } else {
+                if (map.size() == k) {
+                    res = Math.max(res, end - start);
+                    //full map, need to remove the first character in ths substring
+                    for (int index = start; index < end; index++) {
+                        map.put(s.charAt(index), map.get(s.charAt(index))-1);
+                        start++;
+                        if (map.get(s.charAt(index)) == 0) {
+                            //have removed all occurance of a char
+                            map.remove(s.charAt(index));
+                            break;
+                        }
+                    }
+                }
+                map.put(ch, 1);
+            }
+        }
+        res = Math.max(res, s.length() - start);
+        return res;
+    }
+}
