db88ad022c29fc27063b1e7e1a1230e63fbaa0cd
Sugi Venugeethan <sugi205@gmail.com>
2016-04-25 00:08:36 +0000

    [LeetCode Sync] Runtime - 618 ms (60.85%), Memory - N/A (N/A)

---
+++ my-solutions/0336-palindrome-pairs/solution.java
@@ -0,0 +1,48 @@
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> result = new LinkedList<>();
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && isPalindrome(words[i], words[j])) {
+                    List<Integer> item = new ArrayList<Integer>();
+                    item.add(i);
+                    item.add(j);
+                    result.add(item);
+                }
+            }
+        }
+        return result;
+    }
+    
+    public boolean isPalindrome(String word1, String word2) {
+        int i = 0;
+        int j = word2.length() - 1;
+        while (i < word1.length() && j >= 0) {
+            if (word1.charAt(i) != word2.charAt(j)) {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+        if (word1.length() < word2.length()) {
+            i = 0;
+            while (i < j) {
+                if (word2.charAt(i) != word2.charAt(j)) {
+                    return false;
+                }
+                i += 1;
+                j -= 1;
+            }
+        } else if (word1.length() > word2.length()) {
+            j = word1.length() - 1;
+            while (i < j) {
+                if (word1.charAt(i) != word1.charAt(j)) {
+                    return false;
+                }
+                i += 1;
+                j -= 1;
+            }
+        }
+        return true;
+    }
+}
