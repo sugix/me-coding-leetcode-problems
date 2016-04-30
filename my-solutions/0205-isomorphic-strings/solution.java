f5d9a187fc05fcf9a970de4c2afc0ceacdd14051
Sugi Venugeethan <sugi205@gmail.com>
2016-04-30 23:42:15 +0000

    [LeetCode Sync] Runtime - 97 ms (5.06%), Memory - N/A (N/A)

---
+++ my-solutions/0205-isomorphic-strings/solution.java
@@ -0,0 +1,40 @@
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+       if(s==null || t==null)
+        return false;
+ 
+    if(s.length() != t.length())
+        return false;
+ 
+    if(s.length()==0 && t.length()==0)
+        return true;
+ 
+    HashMap<Character, Character> map = new HashMap<Character,Character>();
+    for(int i=0; i<s.length(); i++){
+        char c1 = s.charAt(i);
+        char c2 = t.charAt(i);
+ 
+        Character c = getKey(map, c2);
+        if(c != null && c!=c1){
+            return false;
+        }else if(map.containsKey(c1)){
+            if(c2 != map.get(c1))
+                return false;
+        }else{
+            map.put(c1,c2);
+        }
+    }
+ 
+    return true; 
+    }
+    
+    public Character getKey(HashMap<Character,Character> map, Character target){
+    for (Map.Entry<Character,Character> entry : map.entrySet()) {
+        if (entry.getValue().equals(target)) {
+            return entry.getKey();
+        }
+    }
+ 
+    return null;
+}
+}
