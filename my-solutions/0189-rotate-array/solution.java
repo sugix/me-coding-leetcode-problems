4b3fe63debc7a0a31500dc1df87adbe9f07cac2e
Sugi Venugeethan <sugi205@gmail.com>
2016-04-30 23:10:05 +0000

    [LeetCode Sync] Runtime - 1 ms (50.22%), Memory - N/A (N/A)

---
+++ my-solutions/0189-rotate-array/solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public void rotate(int[] nums, int k) {
+        if (nums == null || nums.length==0 || k < 0) {
+		    throw new IllegalArgumentException("Illegal argument!");
+	    }
+ 
+	    if(k > nums.length) {
+		    k = k % nums.length;
+	    }
+	    
+	    int a = nums.length - k; 
+ 
+	reverse(nums, 0, a-1);
+	reverse(nums, a, nums.length-1);
+	reverse(nums, 0, nums.length-1);
+    }
+    
+    public static void reverse(int[] arr, int left, int right){
+	if(arr == null || arr.length == 1) 
+		return;
+ 
+	while(left < right){
+		int temp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = temp;
+		left++;
+		right--;
+	}	
+}
+}
