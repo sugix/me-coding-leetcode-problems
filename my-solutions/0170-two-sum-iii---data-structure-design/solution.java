f045d42ae17d568df8ca3aa6d393961a3c79c5cb
Sugi Venugeethan <sugi205@gmail.com>
2016-05-01 23:25:54 +0000

    [LeetCode Sync] Runtime - 423 ms (5.72%), Memory - N/A (N/A)

---
+++ my-solutions/0170-two-sum-iii---data-structure-design/solution.java
@@ -0,0 +1,30 @@
+public class TwoSum {
+    private HashMap<Integer, Integer> elements = new HashMap<Integer, Integer>();
+ 
+	public void add(int number) {
+		if (elements.containsKey(number)) {
+			elements.put(number, elements.get(number) + 1);
+		} else {
+			elements.put(number, 1);
+		}
+	}
+ 
+	public boolean find(int value) {
+		for (Integer i : elements.keySet()) {
+			int target = value - i;
+			if (elements.containsKey(target)) {
+				if (i == target && elements.get(target) < 2) {
+					continue;
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+
+// Your TwoSum object will be instantiated and called as such:
+// TwoSum twoSum = new TwoSum();
+// twoSum.add(number);
+// twoSum.find(value);
