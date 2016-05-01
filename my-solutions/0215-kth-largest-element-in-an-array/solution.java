9090d210d7133cc9a82524810b9c85210ffbfe28
Sugi Venugeethan <sugi205@gmail.com>
2016-05-01 20:35:48 +0000

    [LeetCode Sync] Runtime - 19 ms (94.75%), Memory - N/A (N/A)

---
+++ my-solutions/0215-kth-largest-element-in-an-array/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
+    for(int i: nums){
+        q.offer(i);
+ 
+        if(q.size()>k){
+            q.poll();
+        }
+    }
+ 
+    return q.peek();
+    }
+}
