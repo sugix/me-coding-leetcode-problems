cf7290937ccd208ea345eadaca5680e76bc034a4
Sugi Venugeethan <sugi205@gmail.com>
2016-05-03 06:35:41 +0000

    [LeetCode Sync] Runtime - 67 ms (76.18%), Memory - N/A (N/A)

---
+++ my-solutions/0220-contains-duplicate-iii/solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if (k < 1 || t < 0)
+		return false;
+ 
+	TreeSet<Integer> set = new TreeSet<Integer>();
+ 
+	for (int i = 0; i < nums.length; i++) {
+		int c = nums[i];
+		if ((set.floor(c) != null && c <= set.floor(c) + t)
+		|| (set.ceiling(c) != null && c >= set.ceiling(c) -t))
+			return true;
+ 
+		set.add(c);
+ 
+		if (i >= k)
+			set.remove(nums[i - k]);
+	}
+ 
+	return false;
+    }
+}
