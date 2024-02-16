208ebba9ed412640df58af1db0114403c2c0f74d
Sugi Venugeethan <sugi205@gmail.com>
2024-02-16 23:04:31 +0000

    [LeetCode Sync] Runtime - 37 ms (93.68%), Memory - 16.6 MB (45.62%)

---
+++ my-solutions/0013-roman-to-integer/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_lookup = {'I': 1, 'V': 5, 'X': 10,
+                 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        prev, total = 0,0
+        
+        for c in s:
+            curr = roman_lookup[c]
+            total += curr
+
+            if curr > prev:
+                total -= 2 * prev
+
+            prev = curr
+
+        return total
