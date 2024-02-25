b1d20e8706acd7b09353c0f6004c37a67ce71d72
Sugi Venugeethan <sugi205@gmail.com>
2024-02-25 02:33:29 +0000

    [LeetCode Sync] Runtime - 39 ms (23.94%), Memory - 16.5 MB (44.82%)

---
+++ my-solutions/0157-read-n-characters-given-read4/solution.py
@@ -0,0 +1,42 @@
+"""
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+
+        buf_pointer = 0
+        while n > 0:
+
+            buf4 = [''] * 4
+            l = read4(buf4)
+
+            if l == 0: # no more characters to read
+                break
+
+            for j in range(min(l, n)): # remaining n we need or remaining left
+                buf[buf_pointer] = buf4[j]
+                buf_pointer += 1
+                n -= 1
+
+        return buf_pointer
+
+            
+
+
