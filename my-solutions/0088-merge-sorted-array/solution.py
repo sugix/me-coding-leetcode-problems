dcae3adcc2f6cc9d67db097fe527daaeeb76d319
Sugi Venugeethan <sugi205@gmail.com>
2024-08-19 18:49:55 +0000

    [LeetCode Sync] Runtime - 42 ms (40.53%), Memory - 16.5 MB (51.39%)

--- my-solutions/0088-merge-sorted-array/solution.py
+++ my-solutions/0088-merge-sorted-array/solution.py
@@ -1,18 +1,10 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        if n == 0:
-            return
-        if m == 0:
-            nums1[:] = nums2[:]
-            return
-
         p1 = m - 1
         p2 = n - 1
         p = m + n - 1
-
+        
+        # Merge elements from the end of nums1 and nums2
         while p1 >= 0 and p2 >= 0:
             if nums1[p1] > nums2[p2]:
                 nums1[p] = nums1[p1]
@@ -20,11 +12,9 @@ class Solution:
             else:
                 nums1[p] = nums2[p2]
                 p2 -= 1
-
             p -= 1
+
+        if nums1 or nums2:
         
-        if p2 >= 0:
-            nums1[:p2+1]  = nums2[:p2+1]
-
-
-        return
+            # Copy remaining elements from nums2 to nums1 if any
+            nums1[:p2 + 1] = nums2[:p2 + 1]
