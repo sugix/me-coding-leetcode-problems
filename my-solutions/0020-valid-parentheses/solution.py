15bedf2b7ac40062ff97e2330b7c5acf27995992
Sugi Venugeethan <sugi205@gmail.com>
2024-08-19 22:37:46 +0000

    [LeetCode Sync] Runtime - 25 ms (97.45%), Memory - 16.6 MB (10.81%)

--- my-solutions/0020-valid-parentheses/solution.py
+++ my-solutions/0020-valid-parentheses/solution.py
@@ -1,21 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-
-        # Define list as stack since it allows to get last element as first with pop method
-        # Define a mapping between opening and closing brackets. 
-
         stack = []
-        mappings = {")": "(", "}": "{", "]": "["}
+        mapping = {")": "(", "}": "{", "]": "["}
 
         for char in s:
-
-            # if not in mappings then append to stack
-            if char not in mappings:
-                stack.append(char)
-            else:
-                top_element = stack.pop() if stack else "#"
-                if mappings[char] != top_element:
+            if char in mapping:
+                top_element = stack.pop() if stack else '#'
+                if mapping[char] != top_element:
                     return False
+            else:
+                stack.append(char)
 
         return not stack
-        
