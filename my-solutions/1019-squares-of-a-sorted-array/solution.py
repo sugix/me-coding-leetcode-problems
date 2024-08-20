0bbb709a0e8721cf42901a76f9bdc468e4544e76
Sugi Venugeethan <sugi205@gmail.com>
2024-08-20 17:35:33 +0000

    [LeetCode Sync] Runtime - 153 ms (62.05%), Memory - 18.8 MB (26.31%)

--- my-solutions/1019-squares-of-a-sorted-array/solution.py
+++ my-solutions/1019-squares-of-a-sorted-array/solution.py
@@ -1,18 +1,20 @@
-import bisect
-
 class Solution:
-    def sortedSquares(self, A: List[int]) -> List[int]:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
         
-        right = bisect.bisect_left(A, 0)
-        left = right-1
-        result = []
-        while 0 <= left or right < len(A):
-            if right == len(A) or \
-               (0 <= left and A[left]**2 < A[right]**2):
-                result.append(A[left]**2)
-                left -= 1
+        ## Two pointers approach for linear complexity
+        ## Linear time complexity
+        
+        n = len(nums)
+        result = [0] * n
+        left = 0
+        right = n - 1
+
+        for i in range(n - 1, -1, -1):
+            if abs(nums[left]) > abs(nums[right]):
+                result[i] = nums[left] ** 2
+                left += 1
             else:
-                result.append(A[right]**2)
-                right += 1
+                result[i] = nums[right] ** 2
+                right -= 1
+
         return result
-        
