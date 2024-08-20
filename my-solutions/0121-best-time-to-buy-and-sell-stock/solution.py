218ccac05bd70e25889bd8a3444e5ac101d1c8c1
Sugi Venugeethan <sugi205@gmail.com>
2024-08-20 01:02:28 +0000

    [LeetCode Sync] Runtime - 761 ms (36.66%), Memory - 27.3 MB (85.98%)

--- my-solutions/0121-best-time-to-buy-and-sell-stock/solution.py
+++ my-solutions/0121-best-time-to-buy-and-sell-stock/solution.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        # Need to keep track of min price and max profit we can get
         min_price = math.inf
         max_profit = 0
 
         for price in prices:
 
-            min_price = min(min_price, price)
+            min_price = min(price, min_price)
             max_profit = max(max_profit, price - min_price)
 
         return max_profit
-
-        
