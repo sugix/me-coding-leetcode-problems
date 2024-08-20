eebd6055bfa4f365f72f9a79a480938ba2d12c3f
Sugi Venugeethan <sugi205@gmail.com>
2024-08-20 18:42:09 +0000

    [LeetCode Sync] Runtime - 34 ms (77.59%), Memory - 16.4 MB (97.17%)

---
+++ my-solutions/0314-binary-tree-vertical-order-traversal/solution.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q = deque([(root, 0)])
+        d = defaultdict(list)
+        while q:
+            node, col = q.popleft()
+            d[col].append(node.val)
+            if node.left:
+                q.append((node.left, col - 1))
+            if node.right:
+                q.append((node.right, col + 1))
+        return [d[k] for k in sorted(d)]
